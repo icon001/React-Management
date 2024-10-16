71512d6dab3491b9ee1d74f98d004784057fe6af
icon001 <icon001.naver.com>
2024-10-16 11:25:55 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,10 @@ import Table from '@mui/material/Table';
 import TableHead from '@mui/material/TableHead';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
+import CircularProgress from '@mui/material/CircularProgress';
 import { withStyles } from '@mui/styles';
 import Paper from '@mui/material/Paper';
+import { TableRow } from '@mui/material';
 
 const styles =({
   root:{
@@ -17,20 +19,26 @@ const styles =({
   table:{
     minWidth:1080,
   },
+  progress:{
+    margin:'16px'
+  }
 })
 
 
 class App extends Component {
   state = { //state는 변경 될 수 있는 데이터를 명시
-    customers : ""
+    customers : "",
+    completed : 0
   }
   //실제로 API 서버에 접근해서 데이터를 가져 온다.
   componentDidMount(){
     //모든 컴포넌트가 완료 되었을 때 구동되는 생명 주기
+    this.timer = setInterval(this.progress,800); //0.8초 마다 progress 실행
+    
     this.callApi()
       .then(res=> this.setState({customers:res})) //받아온 res 데이터를 customers에 셋팅  
       .catch(err=>console.log(err)); //에러가 발생하는 경우 console에 출력
-
+    
   }
   callApi = async() =>{
     //비동기로 처리 하기 위해서 async로 처리
@@ -38,6 +46,12 @@ class App extends Component {
     const body = await response.json(); //데이터를 json 형태로 가져와서 body 에 저장
     return body;
   }
+
+  progress =() =>{
+    const {completed} = this.state; //state에서 completed 변수를 가져 와서
+    this.setState({completed:completed >= 100 ? 0 : completed + 10}); //completed가 100이 되면 다시 0으로 변경 아니라면 1씩 증가
+  }
+
   render() {
     const { classes } = this.props; //props는 변경 될 수 없는 데이터를 명시
     return (
@@ -63,7 +77,14 @@ class App extends Component {
                   gender={c.gender}
                   job={c.job}
                 />
-              )):""}
+              )):
+              <TableRow>
+                <TableCell colSpan="6" align='center'>
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                </TableCell>
+                
+              </TableRow>
+              }
             </TableBody>
           </Table>
         </Paper>
